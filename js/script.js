c83867375549067d64c1fefc9a02035387fe4280
Noah Murphy <ncm0897@gmail.com>
2024-07-12 12:00:05 -0700

    game board created and styled. keyboard created and still needs styling.

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+const numberOfGuesses = 5;
+/*---------------------------- Variables (state) ----------------------------*/
+let guessesRemaining = numberOfGuesses;
+let guess = [];
+let nextLetter = 0;
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+function init() {
+    let board = document.getElementById('game-board');
+    
+    for (let i = 0; i < numberOfGuesses; i++) {
+        let row = document.createElement('div');
+        row.className = 'word-row';
+
+        for (let k = 0; k < 5; k++) {
+            let box = document.createElement('div');
+            box.className = 'letter-box';
+            row.appendChild(box);
+        }
+        
+        board.appendChild(row);
+    }
+}
+
+init();
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
